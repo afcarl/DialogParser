042a84df2b1023d8a4f8a9b04a9a74e476408915
snakeztc <zhaotiancheng.hz@gmail.com>
2016-06-04 20:35:59 -0400

    working top down parse tree

--- DialogParser.py
+++ DialogParser.py
@@ -9,7 +9,50 @@ class DialogParser(object):
     transform_root = "TRANSFORM_ROOT"
     chart = None
     null_set = None
-    start_rule = (('S', ['START', 'eos']), 0, 0, '', 'predict')
+    is_top_down = False
+    start_rule = None
+
+    RULE = 0
+    DOT = 1
+    POS = 2
+    PARSE = 3
+    OPERATOR = 5
+
+    LHS = 0
+    RHS = 1
+
+    @staticmethod
+    def is_dummy(token):
+        return token == "epsilon" or token == "pop"
+
+    @staticmethod
+    def expect_input(token):
+        extra_set = ['ask_repeat', 'ask_rephrase']
+        return 'request' in token or 'explicit_confirm' in token or token in extra_set
+
+    @staticmethod
+    def is_recursive(token):
+        return '*' in token
+
+    @staticmethod
+    def is_terminal(token):
+        return token.islower()
+
+    @staticmethod
+    def normalize_input(x, with_eos=True):
+        norm_x = [t for t in x if not DialogParser.is_dummy(t)]
+        if with_eos:
+            norm_x.append("eos")
+        return norm_x
+
+    def incomplete(self, s):
+        return len(s[self.RULE][self.RHS]) > 0 and s[self.DOT] < len(s[self.RULE][self.RHS])
+
+    def dot_terminal(self, s):
+        if self.incomplete(s):
+            return DialogParser.is_terminal(s[self.RULE][self.RHS][s[self.DOT]])
+        else:
+            return True
 
     def load_grammar_from_path(self, path):
         f = open(path, 'rb')
@@ -37,62 +80,6 @@ class DialogParser(object):
             self.grammar[lhs] = norm_rules
         print "load " + str(len(self.grammar)) + " rules"
 
-    def completer(self, s, idx):
-        pos = s[2]
-        c = self.chart[pos]
-        lhs = s[0][0]
-        parse = s[3]
-        modified = False
-
-        for ss in c:
-            ss_lhs = ss[0][0]
-            dot = ss[1]
-            if self.incomplete(ss):
-                dot_symbol = ss[0][1][dot]
-                if dot_symbol == lhs:
-                    if DialogParser.is_recursive(lhs) and lhs == ss_lhs:
-                        parse_symbol = parse.strip()
-                    else:
-                        parse_symbol = '(' + lhs + ' ' + parse.strip() + ')'
-
-                    new_s = (ss[0], ss[1]+1, ss[2], ss[3] + parse_symbol, 'complete')
-                    modified = self.add_to_chart(new_s, idx)
-
-        return modified
-
-    def scanner(self, s, idx, tokens):
-        dot = s[1]
-        dot_symbol = s[0][1][dot]
-
-        if dot_symbol != "eos":
-            parse_symbol = dot_symbol + ' '
-        else:
-            parse_symbol = ""
-
-        modified = False
-        if (idx < len(tokens) and dot_symbol == tokens[idx]) or DialogParser.is_dummy(dot_symbol):
-            if DialogParser.is_dummy(dot_symbol):
-                chart_idx = idx
-            else:
-                chart_idx = idx + 1
-
-            new_s = (s[0], s[1] + 1, s[2], s[3] + parse_symbol, 'scanner')
-            modified = self.add_to_chart(new_s, chart_idx)
-
-        return modified
-
-    def predictor(self, s, idx):
-        dot = s[1]
-        dot_symbol = s[0][1][dot]
-        s_rules = self.grammar[dot_symbol]
-        modified = False
-        for sr in s_rules:
-            new_s = ((dot_symbol, sr), 0, idx, '', 'predict')
-            added = self.add_to_chart(new_s, idx)
-            if not modified and added:
-                modified = True
-        return modified
-
     """
     Helper
     """
@@ -107,60 +94,27 @@ class DialogParser(object):
         parse_trees = None
         if self.chart is not None and len(self.chart[-1]) > 0:
             if in_string:
-                parse_trees = [t[3] for t in self.chart[-1] if t[0][0] == 'S']
+                parse_trees = [t[self.PARSE] for t in self.chart[-1] if t[self.RULE][self.LHS] == 'S']
             else:
-                parse_trees = [nltk.Tree.fromstring(t[3]) for t in self.chart[-1] if t[0][0] == 'S']
+                parse_trees = [nltk.Tree.fromstring(t[self.PARSE]) for t in self.chart[-1] if t[self.RULE][self.LHS] == 'S']
         return parse_trees
-
-    def find_all_nullable(self):
-        if self.grammar is None:
-            print "Error: call find null set before read grammmar"
-            return
-        self.null_set = {}
-        prev_size = -1
-        while prev_size != len(self.null_set):
-            prev_size = len(self.null_set)
-
-            for lhs, rules in self.grammar.iteritems():
-                null_parse = self.null_set.get(lhs, [])
-                for rhs in rules:
-                    all_null = True
-                    parse = [""]
-                    for token in rhs:
-                        if DialogParser.is_terminal(token):
-                            if not DialogParser.is_dummy(token):
-                                all_null = False
-                                break
-                            else:
-                                for idx in range(len(parse)):
-                                    parse[idx] += token + " "
-
-                        else:
-                            if token not in self.null_set.keys():
-                                all_null = False
-                                break
-                            else:
-                                all_ways = self.null_set.get(token, [])
-                                parse = parse * len(all_ways)
-                                for idx, way in enumerate(all_ways):
-                                    parse[idx] += "(" + token + " " + way + ")"
-                    if all_null:
-                        null_parse.extend(set(parse))
-
-                if len(null_parse) > 0:
-                    self.null_set[lhs] = list(set(null_parse))
-
-        print "Null set size is " + str(len(self.null_set))
 
     """
     Print methods
     """
+    def print_last_chart(self):
+        print len(self.chart)
+        c = self.chart[-1]
+        for cc in c:
+            if cc[self.RULE][self.LHS] == "START":
+                print cc
+
     def print_chart(self, with_parse=True):
         for idx, c in enumerate(self.chart):
             print idx
             for cc in c:
                 if not with_parse:
-                    print (cc[0], cc[1], cc[2], cc[4])
+                    print (cc[self.RULE], cc[self.DOT], cc[self.POS], cc[self.OPERATOR])
                 else:
                     print cc
 
@@ -179,39 +133,85 @@ class DialogParser(object):
                 print r
             print "-------------"
 
-    @staticmethod
-    def is_dummy(token):
-        return token == "epsilon" or token == "pop"
+    """
+    Main Functions
+    """
 
-    @staticmethod
-    def expect_input(token):
-        extra_set = ['ask_repeat', 'ask_rephrase']
-        return 'request' in token or 'explicit_confirm' in token or token in extra_set
-
-    @staticmethod
-    def is_recursive(token):
-        return '*' in token
-
-    @staticmethod
-    def incomplete(s):
-        return len(s[0][1]) > 0 and s[1] < len(s[0][1])
-
-    @staticmethod
-    def is_terminal(token):
-        return token.islower()
-
-    @staticmethod
-    def dot_terminal(s):
-        if DialogParser.incomplete(s):
-            return DialogParser.is_terminal(s[0][1][s[1]])
+    def __init__(self, parse_type):
+        super(DialogParser, self).__init__()
+        if parse_type == "top_down":
+            self.is_top_down = True
+            self.start_rule = (('S', ['START', 'eos']), 0, 0, '(S START eos)', 'predict')
         else:
-            return True
+            self.is_top_down = False
+            self.start_rule = (('S', ['START', 'eos']), 0, 0, '', 'predict')
 
-    @staticmethod
-    def normalize_input(x):
-        norm_x = [t for t in x if not DialogParser.is_dummy(t)]
-        norm_x.append("eos")
-        return norm_x
+    def completer(self, s, idx):
+        c = self.chart[s[self.POS]]
+        lhs = s[self.RULE][self.LHS]
+        rhs = s[self.RULE][self.RHS]
+        parse = s[self.PARSE]
+        modified = False
+
+        for ss in c:
+            ss_lhs = ss[self.RULE][self.LHS]
+            dot = ss[self.DOT]
+            if self.incomplete(ss):
+                dot_symbol = ss[self.RULE][self.RHS][dot]
+                if dot_symbol == lhs:
+                    if self.is_top_down:
+                        parse_symbol = ss[self.PARSE]
+                        parse_symbol = parse_symbol.replace(lhs, parse)
+                    else:
+                        if DialogParser.is_recursive(lhs) and lhs == ss_lhs:
+                            parse_symbol = ss[self.PARSE] + parse.strip()
+                        else:
+                            parse_symbol = ss[self.PARSE] + '(' + lhs + ' ' + parse.strip() + ')'
+
+                    new_s = (ss[self.RULE], ss[self.DOT] + 1, ss[self.POS], parse_symbol, 'complete')
+                    modified = self.add_to_chart(new_s, idx)
+
+        return modified
+
+    def scanner(self, s, idx, tokens):
+        dot = s[self.DOT]
+        dot_symbol = s[self.RULE][self.RHS][dot]
+
+        if self.is_top_down:
+            parse_symbol = ""
+        else:
+            if dot_symbol != "eos":
+                parse_symbol = dot_symbol + ' '
+            else:
+                parse_symbol = ""
+
+        modified = False
+        if (idx < len(tokens) and dot_symbol == tokens[idx]) or DialogParser.is_dummy(dot_symbol):
+            if DialogParser.is_dummy(dot_symbol):
+                chart_idx = idx
+            else:
+                chart_idx = idx + 1
+
+            new_s = (s[self.RULE], s[self.DOT] + 1, s[self.POS], s[self.PARSE] + parse_symbol, 'scanner')
+            modified = self.add_to_chart(new_s, chart_idx)
+
+        return modified
+
+    def predictor(self, s, idx):
+        dot = s[self.DOT]
+        dot_symbol = s[self.RULE][self.RHS][dot]
+        s_rules = self.grammar[dot_symbol]
+        modified = False
+        for sr in s_rules:
+            if self.is_top_down:
+                parse_symbol = "(" + dot_symbol + " " + " ".join(sr) + ")"
+            else:
+                parse_symbol = ""
+            new_s = ((dot_symbol, sr), 0, idx, parse_symbol, 'predict')
+            added = self.add_to_chart(new_s, idx)
+            if not modified and added:
+                modified = True
+        return modified
 
     def parse(self, x):
         norm_x = self.normalize_input(x)
@@ -236,6 +236,49 @@ class DialogParser(object):
                     if not ever_updated and updated:
                         ever_updated = True
                 stable = not ever_updated
+        return self.chart
 
+    def inc_parse(self, x):
+        """
+        Parse a partial dialog with the last turn scanned
+        :param x:
+        :return:
+        """
+        norm_x = self.normalize_input(x, with_eos=False)
+        self.chart = [[] for i in range(len(norm_x)+1)]
+        # (lhs, rhs), dot, pos, parse, operator_type
+        self.chart[0] = [self.start_rule]
+
+        for i in range(len(self.chart)-1):
+            stable = False
+            while not stable:
+                ever_updated = False
+                s_idx = 0
+                while s_idx < len(self.chart[i]):
+                    s = self.chart[i][s_idx]
+                    if self.incomplete(s) and not self.dot_terminal(s):
+                        updated = self.predictor(s, i)
+                    elif self.incomplete(s) and self.dot_terminal(s):
+                        updated = self.scanner(s, i, norm_x)
+                    else:
+                        updated = self.completer(s, i)
+                    s_idx += 1
+                    if not ever_updated and updated:
+                        ever_updated = True
+                stable = not ever_updated
+
+        # conduct completer on the last chart
+        stable = False
+        i = len(self.chart)-1
+        while not stable:
+            ever_updated = False
+            s_idx = 0
+            while s_idx < len(self.chart[i]):
+                s = self.chart[i][s_idx]
+                updated = self.completer(s, i)
+                s_idx += 1
+                if not ever_updated and updated:
+                    ever_updated = True
+                stable = not ever_updated
         return self.chart
 
--- Main.py
+++ Main.py
@@ -5,15 +5,17 @@ from nltk.draw.tree import TreeView
 import nltk
 
 x = ['inform-my_name', 'inform-welcome', 'request-weather',
-     'request-geography_city', 'implicit_confirm-geography_city','inform-weather']
+     'request-geography_city', 'implicit_confirm-geography_city', 'explicit_confirm-datetime_date', 'inform-weather']
 
-p = DialogParser()
+x0 = ['inform-my_name', 'inform-welcome', 'request-weather']
+x1 = ['inform-my_name', 'inform-welcome', 'request-weather', 'explicit_confirm-geography_city']
+x2 = ['inform-my_name', 'inform-welcome', 'request-weather', 'request-geography_city']
+
+p = DialogParser("top_down")
 p.load_grammar_from_path("/Users/Tony/Documents/intellIJWorkSpace/HRL-RavenClawJava/log/grammar.txt")
-print p.print_grammar()
-
 reader = SessionReader()
-#log_path = "/Users/Tony/Documents/intellIJWorkSpace/HRL-RavenClawJava/log/sessions/11001D2016-06-03T22-05-21.log"
-log_path = "/Users/Tony/Documents/intellIJWorkSpace/HRL-RavenClawJava/log/sessions/11001D2016-06-04T00-26-55.log"
+log_path = "/Users/Tony/Documents/intellIJWorkSpace/HRL-RavenClawJava/log/sessions/11001D2016-06-03T22-05-21.log"
+# log_path = "/Users/Tony/Documents/intellIJWorkSpace/HRL-RavenClawJava/log/sessions/11001D2016-06-04T00-26-55.log"
 
 reader.parse_session_log(log_path)
 sys_str_tree = reader.cur_log.get('parseTree')
@@ -21,20 +23,24 @@ sys_tree = nltk.Tree.fromstring(sys_str_tree)
 
 terminals = sys_tree.leaves()
 reader.print_turns()
-print terminals
 
-c = p.parse(x)
-p.print_chart()
+p.inc_parse(x0)
+p.print_last_chart()
+p.inc_parse(x1)
+p.print_last_chart()
+p.inc_parse(x2)
+p.print_last_chart()
+p.parse(x)
 
-p_trees = p.get_parses(False)
+exit()
+p_trees = p.get_parses(in_string=False)
 
 if p_trees is not None:
     TreeView(sys_tree)._cframe.print_to_file('imgs/original.ps')
     print "Found " + str(len(p_trees)) + " trees."
     for idx, tree in enumerate(p_trees):
         TreeView(tree)._cframe.print_to_file('imgs/'+str(idx)+'.ps')
-        if idx > 5:
-            break
+        break
 else:
     print "No parse found!"
     p.print_chart(with_parse=False)
